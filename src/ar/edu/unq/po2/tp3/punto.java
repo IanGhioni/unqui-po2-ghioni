e9413ba26a0d6ba9993331cee2ef62c4246f57fa
Usuario <Usuario@DESKTOP-C3OFIVD>
2022-09-04 20:35:05 -0300

    Probando

--- src/ar/edu/unq/po2/tp3/punto.java
+++ src/ar/edu/unq/po2/tp3/punto.java
@@ -3,4 +3,16 @@ package ar.edu.unq.po2.tp3;
 public class punto {
 	public float x;
 	public float y;
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
 }
